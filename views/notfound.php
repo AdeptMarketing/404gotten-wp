8ac0330bd5c0f77299ce69793beb6714ef14d1b9
Phil Birnie <pbirnie@marketingadept.com>
2013-10-24 14:13:58 -0400

    Ignore .DS_Store files, update iFrame address.

--- views/notfound.php
+++ views/notfound.php
@@ -17,6 +17,6 @@
 			}
 		</style>
 
-		<iframe src="http://demo.marketingadept.com/compassion/live-test/?r=wp" width="100%" height="900" frameborder="0" scrolling="no"></iframe>
+		<iframe src="http://404gotten.org/foundframe/?r=wp" width="100%" height="900" frameborder="0" scrolling="no"></iframe>
 	</body>
 </html>
